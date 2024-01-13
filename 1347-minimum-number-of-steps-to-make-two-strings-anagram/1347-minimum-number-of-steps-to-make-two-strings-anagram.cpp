e795fb6db5a4f8e3a6b70e8d83e9580155bf068b
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2024-01-13 13:18:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        map<char,int>m1;
-        map<char,int>m2;
+        int freq[26]={0};
         int cnt=0;
-        for(auto x:s){
-            m1[x]++;
+        for(char c:s){
+            freq[c-'a']++;
         }
-        for(auto x:t){
-            m2[x]++;
+        for(char c:t){
+            freq[c-'a']--;
         }
-        sort(t.begin(),t.end());
-        for(auto x:t){
-            if(m1.find(x)!=m1.end()){
-                if(m1[x]>0){
-                    m1[x]--;
-                }else{
-                    cnt++;
-                }
-                
-            }else{
-                cnt++;
+        for(int i=0;i<25;i++){
+            if(freq[i]>0){
+                cnt+=freq[i];
             }
         }
         return cnt;
