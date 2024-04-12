4199810b484fc5286500fa517ea145355c01b8d7
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2024-04-13 01:19:01 +0530

    Time: 7 ms (62.25%), Space: 28 MB (81.7%) - LeetHub

---
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int cnt=0;
+        for(auto x:nums){
+            if(x%2==0){
+                cnt++;
+            }
+        }
+        if(cnt>=2){
+            return true;
+        }
+        return false;
+    }
+};
