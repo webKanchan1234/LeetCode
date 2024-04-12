2c265b25d3b3b50fa14f3ccaa41bab02a7954a0c
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2024-04-13 01:35:45 +0530

    Time: 159 ms (6.69%), Space: 7.2 MB (37.1%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n==0 || n==1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int countPrimeSetBits(int left, int right) {
+        int ans=0;
+        for(int i=left;i<=right;i++){
+            int bits=__builtin_popcount(i);
+            cout<<bits<<" ";
+            if(isPrime(bits)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
