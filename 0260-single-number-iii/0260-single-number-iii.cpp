c8571746d23ff4ae8e1285241d2aa2c33dd62703
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2024-05-31 20:28:47 +0530

    Time: 6 ms (69.06%), Space: 12.3 MB (87.17%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        
-        vector<int>ans;
-        int xr=0;
-        int unique=0;
-        for(auto x:nums){
-            xr^=x;
-            if(xr==0){
-                unique=0;
-            }else{
-                unique=x;
+        int firstXorSec = 0, mask = 1, first = 0;
+
+        for(auto& num: nums){
+            firstXorSec ^= num;
+        }
+
+        while(not (firstXorSec & mask)){
+            mask <<= 1;
+        }
+
+        for(auto& num: nums){
+            if(num & mask){
+                first ^= num;
             }
         }
-        ans.push_back(unique);
-        int v1=xr^unique;
-        ans.push_back(v1);
-        return ans;
+
+        return {first, firstXorSec ^ first};
     }
 };
