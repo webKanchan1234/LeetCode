50b331eff8a917c64e114ff3f44fdd542e91714a
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-02-03 11:04:38 +0530

    Time: 12 ms (98.75%), Space: 70.5 MB (47.7%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
+    bool possible(int day,int m,int k,vector<int>& bloomDay){
+        int cnt=0;
+        int num=0;
+        for(int i=0;i<bloomDay.size();i++){
+            if(bloomDay[i]<=day){
+                cnt++;
+            }else{
+                num+=(cnt/k);
+                cnt=0;
+            }
+        }
+        num+=cnt/k;
+        return num>=m;
+    }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if ((long long)m * k > bloomDay.size()) {
-            return -1;
-        }
+        long long val = m * 1ll * k * 1ll;
+        int n = bloomDay.size();
+        if (val > n) return -1;
+        int l=*min_element(bloomDay.begin(),bloomDay.end());
+        int h=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=0;
 
-        int low = 1, high = 1e9;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (canMakeBouquets(bloomDay, m, k, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(possible(mid,m,k,bloomDay)){
+                h=mid-1;
+            }else{
+                l=mid+1;
             }
         }
-
-        return low;
+        if(l>0){
+            return l;
+        }
+        return -1;
     }
 
-private:
-    bool canMakeBouquets(vector<int>& bloomDay, int m, int k, int day) {
-        int total = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            int count = 0;
-            while (i < bloomDay.size() && count < k && bloomDay[i] <= day) {
-                count++;
-                i++;
-            }
-
-            if (count == k) {
-                total++;
-                i--;
-            }
-
-            if (total >= m) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 };
