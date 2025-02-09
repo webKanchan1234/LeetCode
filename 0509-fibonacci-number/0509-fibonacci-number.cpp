e23ac5e65a6d0ed61b0a926a0c97896864cc234c
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-02-09 09:46:22 +0530

    Time: 6 ms (56.88%), Space: 7.8 MB (34.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        int f=0;
-        int s=1;
-        int ans=0;
-        if(n==0){
-            return 0;
+        if(n<=1){
+            return n;
         }
-        if(n<=2){
-            return 1;
-        }
-        for(int i=2;i<=n;i++){
-            ans=f+s;
-            f=s;
-            s=ans;
-        }
-        return ans;
+        return fib(n-1)+fib(n-2);
     }
 };
