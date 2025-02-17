da524cc04ec5cc8d172fd580f66cbdea12855b08
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-02-17 09:18:09 +0530

    Time: 3 ms (46.32%), Space: 6.3 MB (100%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n=x;
+        long long rev=0;
+        if(x<0)return false;
+        while(x>0){
+            rev=(rev*10)+(x%10);
+            x/=10;
+        }
+        if(rev==n)return true;
+        return false;
+    }
+};
