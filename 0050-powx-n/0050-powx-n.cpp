30a155bb8c00401d24e58649f426ba75dd55e247
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-02-27 20:39:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.74%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double myPow(double x, int n) { 
+        return solve(x, (long)n); }
+
+    double solve(double x, long n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return solve(1 / x, -n);
+        if(n%2 ==0)
+            return solve(x*x, n/2);
+        return x*solve(x*x, (n-1)/2); // odd condn x^9 = x * x^8;
     }
 };
