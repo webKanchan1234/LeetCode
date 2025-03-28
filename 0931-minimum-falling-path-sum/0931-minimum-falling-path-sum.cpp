8796b3c4724c23b0bf1b5ffdcea3d942df45f2e3
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-03-28 10:11:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    
+    int helper(int i, int j, int m, vector<vector<int>> &matrix, vector<vector<int>> &dp){
+        if (j < 0 || j >= m)
+        return 1e9;
+        if (i == 0)
+        return matrix[0][j];
+        if (dp[i][j] != -1)
+        return dp[i][j];
+
+        int upr=matrix[i][j]+helper(i - 1, j + 1, m, matrix, dp);
+        int up=matrix[i][j]+helper(i - 1, j, m, matrix, dp);
+        int upl=matrix[i][j]+helper(i - 1, j - 1, m, matrix, dp);
+        return dp[i][j]=min(upr,min(up,upl));
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        
-        for(int j=0;j<m;j++)
-            dp[0][j]=matrix[0][j];
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){       
-                int a = matrix[i][j] + dp[i-1][j];
-                int b = matrix[i][j]; 
-                    if(j-1>=0)
-                        b += dp[i-1][j-1];
-                    else
-                        b+= 1e9;
-                int c = matrix[i][j]; 
-                    if(j+1<m)
-                        c+= dp[i-1][j+1];
-                    else
-                        c+= 1e9;
-                
-                dp[i][j] = min(a,min(b,c));
-            }
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        int mn=INT_MAX;
+        for(int i=0;i<m;i++){
+            int v=helper(n - 1, i, m, matrix, dp);
+            mn=min(mn,v);
         }
-
-        int res = 1e9;
-        
-        for(int k=0;k<m;k++)
-            res = min(res,dp[n-1][k]);
-        
-        return res;
-
+        return mn;
     }
 };
