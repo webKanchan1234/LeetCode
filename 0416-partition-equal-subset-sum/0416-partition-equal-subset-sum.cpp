aa4ea2a98f66b3a6ee3bc4920dfacd452ffb470e
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-04-08 11:06:52 +0530

    Time: 82 ms (76.63%), Space: 20.9 MB (55.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,27 @@
+int dp[10001][200];
 class Solution {
 public:
-    bool subset(vector<int>& nums, int sum,int n){
-        bool dp[n+1][sum+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0){
-                    dp[i][j]=false;
-                }
-                if(j==0){
-                    dp[i][j]=true;
-                }
-            }
-        }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][sum];
+    int K;
+
+    bool f(int i, int sum1, vector<int>& nums) {
+        if (sum1 == K) return 1;
+        if (i <0 || sum1 > K) return 0;
+        if (dp[sum1][i] != -1) return dp[sum1][i];
+        
+        bool take = f(i-1, sum1+nums[i], nums);
+        bool skip = f(i-1, sum1, nums);
+        
+        return dp[sum1][i]=take|skip;
     }
+
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(auto x:nums){
-            sum+=x;
-        }
-        if(sum%2!=0){
-            return false;
-        }
-        return subset(nums,sum/2,n);
+        const int n = nums.size();
+        int sum =accumulate(nums.begin(), nums.end(), 0);
+        if (sum&1) return 0;
+        K=sum/2;
+        
+        memset(dp, -1, K*200*sizeof(int));
+        
+        return f(n-1, 0, nums);
     }
 };
