9a569177a47945b0330110b524b3cada9c139cb5
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-04-21 21:32:14 +0530

    Time: 3 ms (33.1%), Space: 15 MB (60.42%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -5,7 +5,7 @@ public:
         return 1e9;
         if (i == 0)
         return matrix[0][j];
-        if (dp[i][j] != -1)
+        if (dp[i][j] != INT_MAX)
         return dp[i][j];
 
         int upr=matrix[i][j]+helper(i - 1, j + 1, m, matrix, dp);
@@ -16,7 +16,7 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
         int mn=INT_MAX;
         for(int i=0;i<m;i++){
             int v=helper(n - 1, i, m, matrix, dp);
