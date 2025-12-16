f807b34b521f14a0ca1661c3fac38511affe06bf
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2025-12-16 06:13:23 +0530

    Time: 1 ms (86.1%), Space: 47.3 MB (49.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,10 +1,16 @@
 class Solution {
-    public void dfs(int city,int[][] isConnected,boolean[] isvis){
+    public void bfs(int city,int[][] isConnected,boolean[] isvis){
+        Queue<Integer>q=new LinkedList<>();
+        q.offer(city);
         isvis[city]=true;
 
-        for(int i=0;i<isConnected.length;i++){
-            if(isConnected[city][i]==1 && !isvis[i]){
-                dfs(i,isConnected,isvis);
+        while(!q.isEmpty()){
+            int c=q.poll();
+            for(int j=0;j<isConnected.length;j++){
+                if(isConnected[c][j]==1 && !isvis[j]){
+                    isvis[j]=true;
+                    q.offer(j);
+                }
             }
         }
     }
@@ -16,7 +22,7 @@ class Solution {
         for(int i=0;i<n;i++){
             if(!isvis[i]){
                 ans++;
-                dfs(i,isConnected,isvis);
+                bfs(i,isConnected,isvis);
             }
         }
         return ans;
