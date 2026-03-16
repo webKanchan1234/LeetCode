b33afd35d136d5a1b19c054d90a88695403067fb
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-03-16 23:32:10 +0530

    Time: 14 ms (41.35%), Space: 56.5 MB (55.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int helper(int[] coins,int amount,int i,Integer[][] dp){
-        if(amount==0){
-            return 1;
-        }
-        if(amount<0 || i>=coins.length){
-            return 0;
-        }
-
-        if(dp[i][amount]!=null){
-            return dp[i][amount];
-        }
-
-        int take=0;
-        take=helper(coins,amount-coins[i],i,dp);
-        int not=helper(coins,amount,i+1,dp);
-
-        return dp[i][amount]=take+not;
-    }
     public int change(int amount, int[] coins) {
         int n=coins.length;
-        Integer[][] dp=new Integer[n][amount+1];
-        return helper(coins,amount,0,dp);
+        int[][] dp=new int[n+1][amount+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int a=1;a<=amount;a++){
+                dp[i][a]=dp[i-1][a];
+                if(coins[i-1]<=a){
+                    dp[i][a]+=dp[i][a-coins[i-1]];
+                }
+            }
+        }
+
+        return dp[n][amount];
     }
 }
