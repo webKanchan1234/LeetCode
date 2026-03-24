bcbfcd606c617803f062fe5b97a4a52b8bae264c
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-03-24 22:01:43 +0530

    Time: 0 ms (100%), Space: 46.6 MB (37.52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
+        int ans=0;
+        int n=prices.length;
 
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1]){
+                ans+=prices[i]-prices[i-1];
+            }
         }
-
-        return dp[n-1][0];
+        return ans;
     }
 }
