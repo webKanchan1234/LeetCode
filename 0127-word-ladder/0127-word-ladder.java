0393e3664796b66c1426a283445cb490ef98aed9
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-04-02 00:10:59 +0530

    Time: 67 ms (81.7%), Space: 47.9 MB (71.71%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,39 +1,43 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String>s=new HashSet<>(wordList);
-        Queue<String>q=new LinkedList<>();
-        if (!s.contains(endWord)) return 0;
+        // int n=wordList.length;
+        Set<String> s=new HashSet<>(wordList);
+        Queue<String> q=new LinkedList<>();
+
+        int ans=1;
+
+        if(!s.contains(endWord)){
+            return 0;
+        }
 
         q.offer(beginWord);
-        int lev=1;
-        
+
         while(!q.isEmpty()){
             int sz=q.size();
             for(int i=0;i<sz;i++){
                 String w=q.poll();
+                if(w.equals(endWord)) return ans;
+
                 char[] arr=w.toCharArray();
 
                 for(int j=0;j<arr.length;j++){
-                    char org=arr[j];
+                    char orch=arr[j];
 
-                    for(char c='a';c<='z';c++){
-                        if(org==c) continue;
-
-                        arr[j]=c;
-                        String nxt=new String(arr);
-
-                        if (nxt.equals(endWord)) return lev + 1;
-
-                        if (s.contains(nxt)) {
-                            s.remove(nxt); 
-                            q.offer(nxt);
+                    for(char ch='a';ch<='z';ch++){
+                        arr[j]=ch;
+                        String nw=new String(arr);
+                        if(s.contains(nw)){
+                            q.offer(nw);
+                            s.remove(nw);
                         }
                     }
-                    arr[j] = org;
+                    arr[j]=orch;
                 }
             }
-            lev++;
+            ans++;
         }
+
         return 0;
+
     }
 }
