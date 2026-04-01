18280e357f5b8825fd05fcf9641384d6ba69b27a
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-04-01 22:29:01 +0530

    Time: 14 ms (73%), Space: 49.4 MB (30.64%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,42 +1,40 @@
 class Solution {
-
     public int[][] updateMatrix(int[][] mat) {
-        Queue<int[]>q=new LinkedList<>();
         int n=mat.length;
         int m=mat[0].length;
-        int[][] ans=new int[n][m];
-        
+
+        int[][] dist=new int[n][m];
+        Queue<int[]> q=new LinkedList<>();
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    ans[i][j] = 0;
                     q.offer(new int[]{i,j});
-                }else {
-                    ans[i][j] = Integer.MAX_VALUE;
+                }else{
+                    dist[i][j]=-1;
                 }
             }
         }
 
-        int[] ax={-1,1,0,0};
-        int[] ay={0,0,1,-1};
+        int[][] dirs={{-1,0},{0,1},{1,0},{0,-1}};
 
         while(!q.isEmpty()){
-            int[] cell=q.poll();
             int sz=q.size();
-            int r=cell[0];
-            int c=cell[1];
+            int[] curr=q.poll();
 
-            for(int i=0;i<4;i++){
-                int x=r+ax[i];
-                int y=c+ay[i];
-                if(x>=0 && x<n && y>=0 && y<m && ans[x][y]>ans[r][c]+1){
-                    ans[x][y]=ans[r][c]+1;
-                    q.offer(new int[]{x,y});
+            int r=curr[0];
+            int c=curr[1];
+
+            for(int[] dir:dirs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<m && dist[nr][nc]==-1 ){
+                    dist[nr][nc]=dist[r][c]+1;
+                    q.offer(new int[]{nr,nc});
                 }
             }
-
         }
-        return ans;
+        return dist;
     }
 }
