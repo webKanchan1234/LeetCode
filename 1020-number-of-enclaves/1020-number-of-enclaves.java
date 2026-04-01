b314bfe0d8119defbf66ede4fb0ba505add27c51
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-04-01 23:20:52 +0530

    Time: 6 ms (99.34%), Space: 60.8 MB (45.47%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,58 +1,40 @@
 class Solution {
-    public int count(int[][] grid) {
-        int cnt = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
+    public void dfs(int r,int c,int[][] grid){
+        int n=grid.length;
+        int m=grid[0].length;
+
+        if(r<0 || r>=n || c<0 || c>=m || grid[r][c]==0){
+            return;
+        }
+
+        grid[r][c]=0;
+        dfs(r-1,c,grid);
+        dfs(r,c+1,grid);
+        dfs(r+1,c,grid);
+        dfs(r,c-1,grid);
+    }
+    public int numEnclaves(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+
+        for(int i=0;i<n;i++){
+            dfs(i,0,grid);
+            dfs(i,m-1,grid);
+        }
+
+        for(int i=0;i<m;i++){
+            dfs(0,i,grid);
+            dfs(n-1,i,grid);
+        }
+
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
                     cnt++;
                 }
             }
         }
         return cnt;
-    }
-
-    public int numEnclaves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1) {
-                q.offer(new int[] { i, 0 });
-            }
-
-            if (grid[i][m - 1] == 1) {
-                q.offer(new int[] { i, m - 1 });
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (grid[0][i] == 1) {
-                q.offer(new int[] { 0, i });
-            }
-
-            if (grid[n - 1][i] == 1) {
-                q.offer(new int[] { n - 1, i });
-            }
-        }
-
-        int[] ax={-1,1,0,0};
-        int[] ay={0,0,1,-1};
-
-        while(!q.isEmpty()){
-            int[] cell=q.poll();
-            int r=cell[0];
-            int c=cell[1];
-            if(r<0 || c<0 || r>=n || c>=m || grid[r][c]==0) continue;
-            grid[r][c]=0;
-
-            for(int i=0;i<4;i++){
-                int x=ax[i]+r;
-                int y=ay[i]+c;
-                q.offer(new int[]{x,y});
-            }
-
-        }
-        return count(grid);
     }
 }
