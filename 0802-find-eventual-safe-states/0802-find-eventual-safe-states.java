6c78be7c28c28c2d501194bcc3494a3f9b60e601
webKanchan1234 <47687839+webKanchan1234@users.noreply.github.com>
2026-04-03 12:56:22 +0530

    Time: 19 ms (53.74%), Space: 61.2 MB (69.32%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,43 +1,47 @@
 class Solution {
-    public boolean dfs(int node, int[][] graph, int[] vis, int[] path, int[] check) {
-        vis[node] = 1;
-        path[node] = 1;
-        check[node] = 0;
-
-        for (int next : graph[node]) {
-            if (vis[next] == 0) {
-                if (dfs(next, graph, vis, path, check) == true) {
-                    return true;
-                }
-
-            } else if (path[next] == 1) {
-                return true;
-            }
-        }
-        check[node] = 1;
-        path[node] = 0;
-        return false;
-    }
-
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        int[] vis = new int[n];
-        int[] path = new int[n];
-        int[] check = new int[n];
+        int n=graph.length;
+        int[] indeg=new int[n];
 
+        List<List<Integer>> rev=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            rev.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int nei:graph[i]){
+                rev.get(nei).add(i);
+                indeg[i]++;
+            }
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.offer(i);
+            }
+        }
+
+        boolean[] safe = new boolean[n];
+        List<Integer> ans=new ArrayList<>();
+
+        int idx=0;
+
+        while(!q.isEmpty()){
+            int node=q.poll();
+            safe[node]=true;
+
+            for(int nei:rev.get(node)){
+                indeg[nei]--;
+                if(indeg[nei]==0){
+                    q.offer(nei);
+                }
+            }
+        }
         for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                dfs(i, graph, vis, path, check);
-            }
+            if (safe[i]) ans.add(i);
         }
-
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < check.length; i++) {
-            if (check[i] == 1) {
-                ans.add(i);
-            }
-        }
-
         return ans;
     }
 }
